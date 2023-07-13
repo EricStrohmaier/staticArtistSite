344126e923c3fb026a8ad7a9056d8607885d0168
Eric Strohmaier <eric.strohmaier00@gmail.com!git config --global user.email eric.strohmaier00@gmail.comgit config --global user.email eric.strohmaier00@gmail.com!>
2023-07-13 18:59:13 +0300

    deployment ready

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@
 function Button() {
   return (
     <div className=' relative mt-8 m-2 flex w-full items-center justify-center'>
-    <a href={"/"} className='w-full lg:mx-10 transition duration-200 hover:bg-pink-400  rounded-md relative  border-2 border-white lg:px-4 sm:px-2 p-2 h-[70px] flex items-center justify-center'>
+    <a href={"https://alinalicht.com/alina-licht-stay-hydrated"} target="_blank" rel="noreferrer"  className='w-full lg:mx-10 transition duration-200 hover:bg-pink-400  rounded-md relative  border-2 border-white lg:px-4 sm:px-2 p-2 h-[70px] flex items-center justify-center'>
         <div className='font-semibold lg:text-2xl sm:text-xl text-white '>
           STREAM // DOWNLOAD
       </div>
--- src/components/MainCard.jsx
+++ src/components/MainCard.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
-import profileImg from '../assets/img/profile.JPG';
+import profileImg from '../assets/img/contactbackground.jpg';
 
 export default function MainCard() {
   return (
-    <main className="h-fit w-full  flex justify-center items-center">
-      <div className="m-0 my-10 border-2 border-white w-4/5 flex flex-col md:flex-row">
-        <div className="lg:w-1/2 w-full">
-          <img src={profileImg} alt="" />
+    <main className="h-fit w-full  flex justify-center items-center ">
+      <div className="relative z-0 flex flex-col md:flex-row">
+      {/* <div className="absolute top-0 left-0 w-full h-full bg-black opacity-40 z-10"></div> */}
+
+        <div className="">
+          <img
+           src={profileImg}
+           alt="Artist"
+             />
         </div>
-        <div className="flex flex-col justify-center items-center lg:w-1/2 w-full">
+        <div className="z-20 absolute flex justify-center items-center ">
+        <div className='flex flex-col justify-center items-center text-white'>
           <div className='mt-4  m-2 text-2xl lg:text-6xl font-bold'>CONTACT</div>
-            <div className='text-xl mb-4 '><a href="mailto:steturina@gmail.com">steturina@gmail.com</a>
-
-</div>
-
-
+            <div className='text-xl mb-4'><a href="mailto:steturina@gmail.com">steturina@gmail.com</a></div>
+        </div>
         </div>
       </div>
     </main>
