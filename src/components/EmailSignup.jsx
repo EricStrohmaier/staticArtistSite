2c4feecb190a43db09fc994dc4f4d5c7b4b67025
Eric Strohmaier <eric.strohmaier00@gmail.com!git config --global user.email eric.strohmaier00@gmail.comgit config --global user.email eric.strohmaier00@gmail.com!>
2023-07-17 18:14:44 +0300

    Email sign-up

--- src/components/EmailSignup.jsx
+++ src/components/EmailSignup.jsx
@@ -1,27 +1,59 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 
+function EmailSignup() {
+  const [email, setEmail] = useState('');
+  const [isSignupSuccessful, setIsSignupSuccessful] = useState(false);
+  const {register,trigger,formState: { errors },} = useForm();
 
-function EmailSignup (){
-    const [email , setEmail] = useState('');
+  const handleSignup = async (e) => {
+
+    console.log("~ e", e);
+    const isValid = await trigger();
+    if (!isValid) {
+      e.preventDefault();
+      return
+    }
+
+    // Perform signup logic here
+    // Assuming signup was successful, set the isSignupSuccessful state to true
+    setIsSignupSuccessful(true);
+  };
+
   return (
-   
-    <div className='mt-4  flex w-full  items-center justify-center '>
-       <div className='rounded-md relative lg:mx-8  w-full lg:w-fit h-fit flex justify-center items-center'>
-          <input 
-            onChange={(e) => {setEmail(e.target.value)}}
-            className='w-full  h-[50px] rounded-md indent-2 md:indent-4 ' 
-            type='email' 
-            placeholder='your@email.com' />
-                      
-            <button onClick={()=>{
-             console.log(email);
-                      
-            }} className=' transition duration-100 ml-3 lg:mt-0 lg:ml-3 p-[10px] bg-pink-400 hover:scale-105 rounded-md text-lg md:text-xl font-thin text-white'>
-                Subscribe
-                </button> 
+    <div className="mt-4 flex flex-col items-center">
+      <div className="rounded-md relative lg:mx-8 w-full lg:w-fit h-fit flex justify-center items-center">
+        <input
+          onChange={(e) => setEmail(e.target.value)}
+          className="w-full h-10 rounded-md pl-2 md:pl-4"
+          type="email"
+          placeholder="your@email.com"
+          
+          {...register("email", {
+                required: true,
+                pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+              })}
+        />
+
+        <button
+          onClick={handleSignup}
+          className="transition duration-100 ml-3 lg:mt-0 lg:ml-3 p-2 bg-pink-400 hover:scale-105 rounded-md text-lg md:text-xl font-thin text-white"
+        >
+          Subscribe
+        </button>
         </div>
+        {errors.email && (
+              <p className={`font-normal tracking-widest text-xl text-red-500 mt-2`}>
+                {errors.email.type === "required" && "This field is required."}
+                {errors.email.type === "pattern" && "Invalid email address."}
+              </p>
+            )}
+            {isSignupSuccessful && (
+        <div className="font-normal tracking-widest text-xl text-green-500 mt-2">Email Sign-up successful!</div>
+      )}
+  
     </div>
-  )
+  );
 }
 
-export default EmailSignup
+export default EmailSignup;
--- src/components/MainCard.jsx
+++ src/components/MainCard.jsx
@@ -1,25 +1,40 @@
 import React from 'react';
-import profileImg from '../assets/img/contactbackground.jpg';
+import webimg from '../assets/img/img_web.jpg';
+import mobileimg from '../assets/img/img_mobile.jpg';
+import useMediaQuery from '../hooks/useMediaQuery';
 
 export default function MainCard() {
+
+  const isDesktop = useMediaQuery("(min-width: 900px)");
+
   return (
     <main className="h-fit w-full  flex justify-center items-center ">
       <div className="relative z-0 flex flex-col md:flex-row">
       {/* <div className="absolute top-0 left-0 w-full h-full bg-black opacity-40 z-10"></div> */}
 
         <div className="h-auto w-full">
-          <img
-     
-           src={profileImg}
-           alt="Artist"
-             />
+          {isDesktop ? (
+            <img
+              src={webimg}
+              alt="Artist"
+              />
+          ) : (
+            <img
+              src={mobileimg}
+              alt="Artist"
+              />
+          )}
+          
         </div>
-        <div className="z-20 absolute  ">
-        <div className='flex flex-col justify-center items-center text-white'>
-          <div className='mt-4  m-2 text-2xl lg:text-6xl font-bold'>CONTACT</div>
-            <div className='text-xl mb-4'><a href="mailto:steturina@gmail.com">steturina@gmail.com</a></div>
+        <div className="z-20 absolute flex justify-center items-center h-full w-full">
+          <div className='flex flex-col justify-center items-center text-white'>
+            <div className='mt-4 m-2 text-2xl lg:text-6xl font-bold'>CONTACT</div>
+            <div className='text-xl mb-4'>
+              <a href="mailto:steturina@gmail.com">steturina@gmail.com</a>
+            </div>
+          </div>
         </div>
-        </div>
+
       </div>
     </main>
   );
